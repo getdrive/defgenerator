e6bf7cd2311935038c536f9bf04d7685a0b0fff3
GetDrive <getdrive@users.noreply.github.com>
2017-04-04 21:40:57 +0300

    Update defgenerator.sh

--- defgenerator.sh
+++ defgenerator.sh
@@ -1,6 +1,6 @@
 !# /bin/bash
 clear
-read -p "Введите def-код мобильного оператора": func
+read -p "Введите def-код мобильного оператора (можно взять здесь: http://8sot.su/ru/regions)": func
 lengh=$(echo -n $func | wc -c)
 if [ "$lengh" = "11" ]; then 
 crunch 11 11 0123456789 -t $func -o $PWD/$func && cat $func >> mobile && rm -rf $func
