f0f4cdeb2a54384b1b0e46d26758014b3c4046fe
ĢēŦĐŗĩɤɘ <getdrive@users.noreply.github.com>
2022-09-27 22:37:46 +0300

    Update defgenerator.sh

--- defgenerator.sh
+++ defgenerator.sh
@@ -3,7 +3,7 @@ clear
 read -p "Введите def-код мобильного оператора (можно взять здесь: http://8sot.su/ru/regions)": func
 lengh=$(echo -n $func | wc -c)
 if [ "$lengh" = "11" ]; then 
-crunch 11 11 0123456789 -t $func -o $PWD/$func && cat $func >> mobile && rm -rf $func
+crunch 11 11 -t $func%%%%%%% -o $PWD/$func && cat $func >> mobile && rm -rf $func
 else
 echo "!!!Ввод должен быть 11 символов!!!"
 sleep 2
